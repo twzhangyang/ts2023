c31ddc127cde83be122babaf6df331e3ff42ce19
Yang <yrzhang@thoughtworks.com>
2023-10-20 21:30:23 +1100

    move all scripts to basics

---
+++ src/Intermediate/readonlyModifer.ts
@@ -0,0 +1,13 @@
+type PointA = {
+    readonly x: number,
+    readonly y: number
+}
+
+const point: PointA = { x: 0, y: 0};
+
+// Variable assignment
+// point = {x: 1, y: 1}
+
+// Property assignment
+// point.x = 1;
+// point.y = 1
