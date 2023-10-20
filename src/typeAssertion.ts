2865516004794b8cdd0ff8a4db1ec2bb072bbe45
Yang <yrzhang@thoughtworks.com>
2023-10-20 20:49:16 +1100

    type assertion and cast

---
+++ src/typeAssertion.ts
@@ -0,0 +1,14 @@
+const load = () => "";
+
+const hello2 = load();
+
+const trimmed = (hello as string).trim();
+
+
+let leet;
+leet = '1233';
+
+// Use as number
+// It will have a compile error
+// const number = leet as number;
+// console.log(number == 1233); //false
