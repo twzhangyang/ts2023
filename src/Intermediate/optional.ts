1435daacd1b28004b6394381750ffb3aea56baac
Yang <yrzhang@thoughtworks.com>
2023-10-21 20:34:59 +1100

    non null assertion

--- src/Intermediate/optional.ts
+++ src/Intermediate/optional.ts
@@ -1,12 +1,26 @@
 type PersonB = {
     name: string,
     email: string,
-    age?:string | null
+    age?: string | null,
+    address?: string
 }
 
 const p: PersonB = {
     name: "yang",
-    email: "test@test.com"
+    email: "test@test.com",
+    address: "address"
 }
 
 console.log(p.age);
+
+const printAddress = (address: string) => {
+    console.log(address);
+}
+
+printAddress(p.address!)
+
+const printAddressWithAssertion = (person: PersonB) => {
+    if(person.address == null) throw new Error("Address is null")
+    printAddress(person.address);
+}
+
